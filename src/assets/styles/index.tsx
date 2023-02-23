0351a57c5ca9b95d202086aba7ff7b0cf7d6fab5
David <david.madrid@soluntech.com>
2023-02-23 18:22:22 -0500

    Fixed navlinks radius in Menu component

--- src/assets/styles/index.tsx
+++ src/assets/styles/index.tsx
@@ -1,4 +1,4 @@
 export let activeStyle = 'p-2 text-white bg-neutral-800 rounded-lg';
 
 export let unactiveStyle =
-  'transition duration-300 p-2 bg-inherit hover:text-white';
+  'transition duration-300 p-2 bg-inherit hover:text-white rounded-lg';
