0aeb1dbbe6522acbb027bf8fbf4c7c0809092e06
David <david.madrid@soluntech.com>
2023-02-24 07:49:15 -0500

    Styled UnexpectedError component

--- src/components/errors/error-404.tsx
+++ src/components/errors/error-404.tsx
@@ -1,5 +1,9 @@
 const Error404 = () => {
-  return <div>Error404 NOT FOUND</div>;
+  return (
+    <div className='text-neutral-400 flex justify-center h-96 items-center'>
+      We couldn't find the page you were looking for...
+    </div>
+  );
 };
 
 export default Error404;
