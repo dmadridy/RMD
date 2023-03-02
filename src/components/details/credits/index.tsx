024638050b1e20f77600bd3c2ec1fed936781868
David Madrid <davidmadridpathway@gmail.com>
2023-03-02 17:11:56 -0500

    Fixed?

--- src/components/details/credits/index.tsx
+++ src/components/details/credits/index.tsx
@@ -11,7 +11,7 @@ const Credits = () => {
 
   if (error) return <DataError />;
 
-  return <div className='container mx-auto max-w-7xl'>Credits</div>;
+  return <div className='container mx-auto max-w-7xl'>The Credits</div>;
 };
 
 export default Credits;
