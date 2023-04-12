c8fe607f608761f1563270f3aebb8de10cd250b9
David Madrid <davidmadridpathway@gmail.com>
2023-04-12 18:52:53 -0500

    Set ESLINT and PRETTIER, im getting errors, needs fixing

--- src/layouts/menu.tsx
+++ src/layouts/menu.tsx
@@ -3,16 +3,13 @@ import { activeStyle, unactiveStyle } from '../assets/styles';
 import user from '../assets/images/user.png';
 import { useSelector } from 'react-redux';
 import { RootState } from '../store';
-import { useState } from 'react';
-import Login from '../components/general/login';
 
 const Menu = () => {
   const username = useSelector((state: RootState) => state.user);
-  const [openForm, setOpenForm] = useState(false);
 
   return (
-    <div className='flex justify-between container mx-auto max-w-7xl pt-8 pb-2 text-neutral-400 overflow-auto items-center font-medium'>
-      <div className='flex gap-2'>
+    <div className='flex justify-between max-w-7xl container mx-auto'>
+      <div className='flex pt-8 mb-10 pb-2 text-neutral-400 overflow-auto items-center font-medium gap-2 whitespace-nowrap'>
         <NavLink
           className={({ isActive }) => (isActive ? activeStyle : unactiveStyle)}
           to='/'
@@ -38,24 +35,24 @@ const Menu = () => {
           Upcoming
         </NavLink>
       </div>
-      <div className='flex gap-2'>
+
+      <div className='flex pt-8 mb-10 pb-2 text-neutral-400 overflow-auto items-center font-medium gap-2 whitespace-nowrap'>
         <NavLink
           className={({ isActive }) => (isActive ? activeStyle : unactiveStyle)}
           to='/reference'
         >
           Reference
         </NavLink>
-        <button
-          onClick={() => setOpenForm(true)}
-          className='flex gap-1 items-center border-2 border-neutral-700 p-1 px-3 rounded-full hover:border-neutral-400 duration-300'
-        >
-          <img className='w-6' src={user} alt='' />
-          {username.status ? username.user : 'Log In'}
-        </button>
-        {openForm && (
-          <div className='fixed inset-0 bg-black bg-opacity-50 z-50 flex items-center justify-center'>
-            <Login setOpenForm={setOpenForm} />
-          </div>
+        {username.user ? (
+          <button className='flex gap-1 items-center border-2 border-neutral-700 p-1 px-3 rounded-full hover:border-neutral-400 duration-300'>
+            <img className='w-6' src={user} alt='' />
+            {username.user}
+          </button>
+        ) : (
+          <button className='flex gap-1 items-center border-2 border-neutral-700 p-1 px-3 rounded-full hover:border-neutral-400 duration-300'>
+            <img className='w-6' src={user} alt='' />
+            Log In
+          </button>
         )}
       </div>
     </div>
